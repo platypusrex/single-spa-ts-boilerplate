dfb24b2e49b8af85c2988bad7d31b24053cf4edd
Frank Cooke <frankcooke79@gmail.com>
2019-07-09 09:31:45 -0400

    fixes single-spa config

--- packages/root/src/single-spa.config.ts
+++ packages/root/src/single-spa.config.ts
@@ -2,14 +2,18 @@ import { registerApplication, start } from 'single-spa';
 
 registerApplication(
   'app1',
-  () => import('app1/src'),
-  (location) => location.pathname.startsWith('/app1')
+  () => import('../../app1/src'),
+  (location) => location.pathname === '' ||
+    location.pathname === '/' ||
+    location.pathname.startsWith('/app1')
 );
 
 registerApplication(
   'app2',
-  () => import('app2/src'),
-  (location) => location.pathname.startsWith('/app2')
+  () => import('../../app2/src'),
+  (location) => location.pathname === '' ||
+    location.pathname === '/' ||
+    location.pathname.startsWith('/app2')
 );
 
 start();
